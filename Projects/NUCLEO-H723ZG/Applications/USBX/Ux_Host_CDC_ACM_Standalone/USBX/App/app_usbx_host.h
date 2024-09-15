ad87eaf06cc864c4b1c4a66057ab8aed79f2d4e1
Mohamed AYED <mohamed.ayed1@outlook.com>
2024-09-15 16:28:06 +0200

    add dual hid app
    add dual hid app

---
+++ Projects/NUCLEO-H723ZG/Applications/USBX/Ux_Host_CDC_ACM_Standalone/USBX/App/app_usbx_host.h
@@ -0,0 +1,74 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    app_usbx_host.h
+  * @author  MCD Application Team
+  * @brief   USBX Host applicative header file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2020-2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __APP_USBX_HOST_H__
+#define __APP_USBX_HOST_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "ux_api.h"
+#include "main.h"
+#include "ux_host_cdc_acm.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+#define USBX_HOST_APP_MEMORY_BUFFER_SIZE  1024
+#define USBX_HOST_MEMORY_STACK_SIZE     1024 * 10
+
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+UINT MX_USBX_Host_Init(VOID);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __APP_USBX_HOST_H__ */
